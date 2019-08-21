e353ad69eea153e9e9f671c3504fbd25dc1a681b
hepuyao <2410474020@qq.com>
2019-08-21 08:37:17 +0800

    0code

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "ukuipanelapplication.h"
-#include "config/configpluginswidget.h"
-int main (int argc, char *argv[])
-{
-	UkuiPanelApplication app(argc, argv);
-//    ConfigPluginsWidget v;
-//    v.show();
-	return app.exec();		
-}

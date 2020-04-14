d20be10856257cc0b19d3c80fc90f663964a911e
hepuyao <2410474020@qq.com>
2020-04-14 14:28:10 +0800

    liushanwen

---
+++ DesktopEnvironment/Dbus/dbus-example-server/main.cpp
@@ -0,0 +1,27 @@
+#include <QtDBus/QDBusConnection>
+#include <QCoreApplication>
+#include <QDebug>
+#include <QDBusError>
+#include <QDBusMetaType>
+#include "dbus_demo_example.h"
+#include "dbus_demo_example_struct.h"
+#include "example_adaptor.h"
+int main(int argc, char *argv[])
+{
+	QCoreApplication a(argc, argv);
+
+	qRegisterMetaType<dbus_demo_example_struct>("dbus_demo_example_struct");
+	qDBusRegisterMetaType<dbus_demo_example_struct>();
+	QDBusConnection connection = QDBusConnection::systemBus();
+
+	if (!connection.registerService("dbus.demo.example")) {
+		qDebug() << "error:" << connection.lastError().message();
+		exit(-1);
+	}
+	
+	dbus_demo_example *dbus_demo = new dbus_demo_example();
+	new IterfaceAdaptor(dbus_demo);
+	connection.registerObject("/dbus/demo/example", dbus_demo);
+	
+	return a.exec();	
+}

45cda3e64b2d06614feb6ca5a6cd82915d2726f4
hepuyao <2410474020@qq.com>
2020-12-30 09:45:03 +0800

    改变透明度

--- sh/changetrans.sh
+++ sh/changetrans.sh
@@ -1,24 +1,13 @@
 #/bin/bash 
 while ((1))
 do
-        gsettings set org.ukui.control-center.personalise transparency 0.1
-	sleep  0.03
-        gsettings set org.ukui.control-center.personalise transparency 0.2
-	sleep 0.01
-        gsettings set org.ukui.control-center.personalise transparency 0.3
-	sleep 0.01
-	gsettings set org.ukui.control-center.personalise transparency 0.4
-        sleep 0.01
-        gsettings set org.ukui.control-center.personalise transparency 0.5
-	sleep 0.01
-	gsettings set org.ukui.control-center.personalise transparency 0.6
-        sleep 0.01
-	gsettings set org.ukui.control-center.personalise transparency 0.7
-        sleep 0.02
-        gsettings set org.ukui.control-center.personalise transparency 0.8
-	sleep 0.01
-        gsettings set org.ukui.control-center.personalise transparency 0.9
-	gsettings set org.ukui.control-center.personalise transparency 1
-        sleep 0.01
+	for((i=1;i<=10;i++));
+do
+	sleep 0.1
+	s=0.`scale=2 expr $i % 10 `;
+	gsettings set org.ukui.control-center.personalise transparency $s
+	gsettings set org.ukui.style peony-side-bar-transparency `expr $i \* 10`
+	gsettings set org.ukui.style menu-transparency `expr $i \* 10`
+done
 done
 
